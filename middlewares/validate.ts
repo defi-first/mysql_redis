8baa115905370930a1e7afe2188a8b4b7622531e
Jack <koyaso@proton.me>
2026-03-07 14:26:08 +0800

    feat: set up zod and middleware

---
+++ middlewares/validate.ts
@@ -0,0 +1,12 @@
+import type { NextFunction, Request, Response } from "express";
+import type { ZodSchema } from "zod";
+
+export const validate =
+	<T>(schema: ZodSchema<T>) =>
+	(req: Request, res: Response, next: NextFunction) => {
+		const result = schema.safeParse(req.body);
+		if (!result.success) {
+			return res.status(400).json({ success: false, errors: result.error });
+		}
+		next();
+	};
