30a4f70542e629113f155f8fe6323e710689b3da
Jack <koyaso@proton.me>
2026-03-07 13:28:35 +0800

    feat: set up errorHandler

---
+++ middlewares/errorHandler.ts
@@ -0,0 +1,7 @@
+import type { NextFunction, Request, Response } from "express";
+import { errorResponse } from "../utils/responses.js";
+
+export function errorHandler(err: any, req: Request, res: Response, next: NextFunction) {
+	console.error(err);
+	errorResponse(res, 500, err);
+}
